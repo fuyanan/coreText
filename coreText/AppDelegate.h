bc2feb5e111b0b544a3f5a5f162884ce3a04ab59
funan <funan@fyn-aa.local>
2015-09-08 14:10:56 +0800

    Initial Commit

---
+++ coreText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coreText
+//
+//  Created by funan on 15/9/8.
+//  Copyright (c) 2015年 fuyanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coreText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreText
+//
+//  Created by funan on 15/9/8.
+//  Copyright (c) 2015年 fuyanan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
